8df85fca0389f4a68714415a08b591d85e2a8e22
Amey Vyas <your-email@example.com>
2025-05-05 17:42:04 +0530

    latest

--- app/api/comments/[communityName]/route.ts
+++ app/api/comments/[communityName]/route.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
 import { NextRequest, NextResponse } from 'next/server';
 import connectMongo from '../../../lib/mongodb';
 import Comment from '../../../models/comments';
